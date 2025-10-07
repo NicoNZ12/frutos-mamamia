ac67241a710a703d458aef6548d86a92b64a9099
Nicolas Nuñez <nuneznicolas582@gmail.com>
2025-10-07 18:49:00 -0300

    Levanta server inicial

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.SERVER_PORT || 3000
+
+//config
+const app = express()
+
+//middlewares
+app.use(express.json())
+
+//endpoint inicial
+app.get("/", (_req, res) => {
+    res.status(200).json({ message: "Server corriendo correctamente!" })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`)
+})
